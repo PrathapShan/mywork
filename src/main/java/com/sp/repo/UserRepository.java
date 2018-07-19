a5bc1f72b39b68835e3f37b17a072cc6d0333f1d
Sadhana <s.prathap@gmail.com>
2018-07-19 13:46:25 +0530

    testing

--- src/main/java/com/sp/repo/UserRepository.java
+++ src/main/java/com/sp/repo/UserRepository.java
@@ -3,7 +3,11 @@ package com.sp.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sp.entity.User;
+import com.sp.entity.User; 
+
+/**
+Prathap
+**/
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
